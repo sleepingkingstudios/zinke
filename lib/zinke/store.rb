8695e0ff677f65089871326e6866b216c7b66ae5
Rob Smith <merlin@sleepingkingstudios.com>
2018-10-17 18:18:39 -0400

    Remove deprecated Zinke::Store::Listener.

--- lib/zinke/store.rb
+++ lib/zinke/store.rb
@@ -2,6 +2,9 @@
 
 require 'observer'
 
+require 'zinke/listeners/base_listener'
+require 'zinke/listeners/type_listener'
+
 module Zinke
   # Encapsulates a single state and provides dispatch and subscribe methods to
   # notify on updates to that state.
@@ -9,6 +12,12 @@ module Zinke
     # Helper class implementing Observable and providing a less cryptic error
     # message when adding a listener while dispatching an action.
     class Dispatcher
+      ITERATION_ERROR = "can't add a new key into hash during iteration"
+      private_constant :ITERATION_ERROR
+
+      # Error message to display when #update tries to add a listener.
+      LISTENER_ERROR = "can't add a listener while dispatching an action"
+
       include Observable
 
       # Dispatches an action. All subscribed listeners will be notified via
@@ -26,6 +35,10 @@ module Zinke
         changed
 
         notify_observers(action)
+      rescue RuntimeError => exception
+        raise unless exception.message == ITERATION_ERROR
+
+        raise LISTENER_ERROR
       end
 
       # Creates and adds an action listener with the specified definition and
@@ -43,15 +56,21 @@ module Zinke
       #   Must define the #call method with one argument, which will be the
       #   dispatched action.
       #
-      # @return [Zinke::Store::Listener] The added listener. To stop notifying
-      #   the listener when a matching action is dispatched, pass this to the
-      #   #unsubscribe method.
+      # @return [Zinke::Listeners::BaseListener] The added listener. To stop
+      #   notifying the listener when a matching action is dispatched, pass this
+      #   to the #unsubscribe method.
       #
       # @see #dispatch
       #
       # @see #unsubscribe
-      def subscribe(action_type = nil, definition:)
-        listener = Listener.new(action_type, definition: definition)
+      def subscribe(action_type = nil, &block)
+        listener =
+          if action_type
+            Zinke::Listeners::TypeListener
+              .new(action_type: action_type, &block)
+          else
+            Zinke::Listeners::BaseListener.new(&block)
+          end
 
         add_observer(listener)
 
@@ -61,60 +80,11 @@ module Zinke
       # Removes a subscribed listener. The listener will no longer be notified
       # when a matching action is dispatched.
       #
-      # @param listener [Zinke::Store::Listener] The listener to remove.
+      # @param listener [Zinke::Listeners::BaseListener] The listener to remove.
       #
       # @see #dispatch
       def unsubscribe(listener)
         delete_observer(listener)
-      end
-    end
-
-    # Helper class encapsulating an action handler. Optionally provides basic
-    # equality check against an expected action type.
-    #
-    # @see Zinke::Store::Dispatcher#subscribe
-    class Listener
-      ITERATION_ERROR = "can't add a new key into hash during iteration"
-      private_constant :ITERATION_ERROR
-
-      # Error message to display when #update tries to add a listener.
-      LISTENER_ERROR = "can't add a listener while dispatching an action"
-
-      # @param action_type [String, Symbol, nil] The type of action to listen
-      #   for. The definition will only be called for dispatched actions of the
-      #   specified type. If no action type is given, the definition will be
-      #   called for all dispatched actions. Defaults to nil.
-      #
-      # @param definition [Proc, #call] An executable object, called by the
-      #   #update method when a matching action is dispatched. Must define the
-      #   #call method with one argument, which will be the dispatched action.
-      def initialize(action_type = nil, definition:)
-        @action_type = action_type
-        @definition  = definition
-      end
-
-      # Checks the type of the action and, if matching the expected action type,
-      # calls the definition with the action.
-      #
-      # @param action [Hash] The action object. By convention, should be a Hash
-      #   with a :type key and a String or Symbol value, and can have other keys
-      #   and values representing additional data.
-      #
-      # @return [Object] The value returned by calling the definition.
-      #
-      # @raise RuntimeError if the definition tries to add another listener.
-      #
-      # @see Zinke::Store::Dispatcher#dispatch
-      #
-      # @see LISTENER_ERROR
-      def update(action)
-        return unless @action_type.nil? || @action_type == action[:type]
-
-        @definition.call(action)
-      rescue RuntimeError => exception
-        raise unless exception.message == ITERATION_ERROR
-
-        raise LISTENER_ERROR
       end
     end
 
@@ -150,15 +120,15 @@ module Zinke
     #   should be a Hash with a :type key and a String or Symbol value, and can
     #   have other keys and values representing additional data.
     #
-    # @return [Zinke::Store::Listener] The added listener. To stop notifying
-    #   the listener when a matching action is dispatched, pass this to the
-    #   #unsubscribe method.
+    # @return [Zinke::Listeners::BaseListener] The added listener. To stop
+    #   notifying the listener when a matching action is dispatched, pass this
+    #   to the #unsubscribe method.
     #
     # @see #dispatch
     #
     # @see #unsubscribe
     def subscribe(action_type = nil, &block)
-      @dispatcher.subscribe(action_type, definition: block)
+      @dispatcher.subscribe(action_type, &block)
     end
 
     # (see Zinke::Store::Dispatcher#unsubscribe)
